9b1eb5bd05000876cf55c93d6285e847fc1a43db
Tom Berthon <github@tomberthon.me.uk>
2015-05-12 18:33:37 +0100

    add echos to cron

--- dist/bin/brightness.sh
+++ dist/bin/brightness.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
 function set_brightness {
+  echo "Redis: $DB_PORT_6379_TCP_ADDR $DB_PORT_6379_TCP_PORT";
   bright_value=$(/opt/train-alerts/libs/redi.sh/redi.sh -g mod-brightness);
+  
+  echo "Brightness: $bright_value";
   gpio -g pwm 18 $bright_value;
 }
 
 function init {
+  echo "gpio init";
   gpio -g mode 18 pwm;
 }
 
